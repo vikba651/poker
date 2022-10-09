2634b5e76825b012dc9e373fd21f6993a75d89e4
Arin Rashid <arin.rashid@hotmail.com>
2022-10-09 18:39:54 +0200

    Test change

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="StartScreen" component={StartScreen} />
+        <Stack.Screen name="StartScreens" component={StartScreen} />
         <Stack.Screen name="SettleUpScreen" component={SettleUpScreen} />
         <Stack.Screen name="TrackGameScreen" component={TrackGameScreen} />
       </Stack.Navigator>
