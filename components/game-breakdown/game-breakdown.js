a0375cc5fd77cfee0c7211b3d9b17713bbf9aee0
Arin Rashid <arin.rashid@hotmail.com>
2022-11-07 10:53:31 +0100

    Styling of breakdown

--- components/game-breakdown/game-breakdown.js
+++ components/game-breakdown/game-breakdown.js
@@ -1,4 +1,4 @@
-import { View, SafeAreaView, Image, TouchableOpacity, Text } from 'react-native'
+import { View, SafeAreaView, Image, TouchableOpacity, Text, ScrollView } from 'react-native'
 import React, { useState } from 'react'
 import styles from './game-breakdown.scss'
 
@@ -28,24 +28,55 @@ export default function GameBreakDown({ navigation, route }) {
   // ]
   const rounds = route.params.allRounds
   return (
-    <SafeAreaView
-      style={{
-        flex: 1,
-        width: '100%',
-        alignItems: 'center',
-      }}
-    >
+    <SafeAreaView style={styles.container}>
       <Text style={{ fontSize: 40, margin: 20 }}>You played {rounds.length} rounds</Text>
-      {rounds.map((round) => {
-        return (
-          <View key={round.round} className={styles.chooseView}>
-            <Text style={{ fontSize: 32 }}>{round.round}</Text>
-            {round.cards.map((card) => {
-              return <Text key={card.id}>{card.value + ' ' + card.suit}</Text>
-            })}
-          </View>
-        )
-      })}
+      <ScrollView className={styles.scrollView}>
+        {rounds.map((round) => {
+          return (
+            <View key={round.round} className={styles.breakdownView}>
+              <Text style={{ fontSize: 32 }}>{round.round}</Text>
+              <View className={styles.cardsView}>
+                <View className={styles.cardsRow}>
+                  {round.cards.slice(0, 2).map((card, i) => {
+                    return (
+                      <View key={card.id} className={styles.playerCard}>
+                        {!!card.suit && (
+                          <Image
+                            className={styles.playerCardSuit}
+                            style={{ resizeMode: 'contain' }}
+                            source={card.suitImage}
+                          />
+                        )}
+                        {!!card.value && <Text className={styles.cardTopValue}>{card.value}</Text>}
+                        {!!card.value && <Text className={styles.cardBottomValue}>{card.value}</Text>}
+                      </View>
+                    )
+                  })}
+                </View>
+                <View className={styles.cardsRow}>
+                  {round.cards.slice(2, 7).map((card, i) => {
+                    return (
+                      <View key={card.id} className={styles.playerCard}>
+                        {!!card.suit && (
+                          <Image
+                            className={styles.playerCardSuit}
+                            style={{ resizeMode: 'contain' }}
+                            source={card.suitImage}
+                          />
+                        )}
+                        {<Text className={styles.cardTopValue}>{card.value}</Text>}
+                        {<Text className={styles.cardBottomValue}>{card.value}</Text>}
+                      </View>
+                    )
+                  })
+
+                  }
+                </View>
+              </View>
+            </View>
+          )
+        })}
+      </ScrollView>
     </SafeAreaView>
   )
 }
