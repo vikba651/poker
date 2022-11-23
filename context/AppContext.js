d239df92fa9fbbca40cc2f1a1cd3c2d8ce182ed2
Arin Rashid <arin.rashid@hotmail.com>
2022-11-23 16:27:03 +0100

    Add names from session to settle up

--- context/AppContext.js
+++ context/AppContext.js
@@ -3,47 +3,47 @@ import { io } from 'socket.io-client'
 
 const AppContext = React.createContext()
 
-const SERVER_ADDR = 'http://192.168.0.11:8020'
+const SERVER_ADDR = 'http://192.168.8.190:8020'
 const socket = io(SERVER_ADDR)
 
 export const AppProvider = ({ children }) => {
-  // const socket = useRef(null)
+    // const socket = useRef(null)
 
-  const [players, setPlayers] = useState([])
-  const [userName, setUserName] = useState('')
-  const [serverState, setServerState] = useState('Loading Websocket...')
-  const [session, setSession] = useState(null)
-  const [location, setLocation] = useState(null)
+    const [players, setPlayers] = useState([])
+    const [userName, setUserName] = useState('')
+    const [serverState, setServerState] = useState('Loading Websocket...')
+    const [session, setSession] = useState(null)
+    const [location, setLocation] = useState(null)
 
-  const providers = {
-    socket,
-    userName,
-    setUserName,
-    players,
-    setPlayers,
-    serverState,
-    setServerState,
-    session,
-    setSession,
-    location,
-    setLocation,
-  }
-
-  useEffect(() => {
-    socket.on('connect', () => {
-      setServerState('Connected to Websocket')
-    })
-
-    socket.on('disconnect', () => {
-      setServerState('Disconnected from Websocket')
-    })
-
-    return () => {
-      socket.disconnect()
+    const providers = {
+        socket,
+        userName,
+        setUserName,
+        players,
+        setPlayers,
+        serverState,
+        setServerState,
+        session,
+        setSession,
+        location,
+        setLocation,
     }
-  }, [socket])
 
-  return <AppContext.Provider value={providers}>{children}</AppContext.Provider>
+    useEffect(() => {
+        socket.on('connect', () => {
+            setServerState('Connected to Websocket')
+        })
+
+        socket.on('disconnect', () => {
+            setServerState('Disconnected from Websocket')
+        })
+
+        return () => {
+            socket.disconnect()
+        }
+    }, [socket])
+
+    return <AppContext.Provider value={providers}>{children}</AppContext.Provider>
 }
 
 export default AppContext
