d35b721d0a1a9332f0ab77b1cb928ba93cdf6b05
Eric Brune <ericjosefbrune@gmail.com>
2023-03-31 21:25:51 +0200

    Initial commit, facelift and delete round

--- screens/stats-screen/stats-screen.js
+++ screens/stats-screen/stats-screen.js
@@ -1,14 +1,17 @@
 import React, { useEffect, useState, useContext } from 'react'
-import { SafeAreaView, Text, TouchableOpacity, View, ActivityIndicator, ScrollView } from 'react-native'
-import { getRounds } from '../../shared/api'
+import { Image, SafeAreaView, Text, TouchableOpacity, View, ActivityIndicator, ScrollView } from 'react-native'
+import { getRounds, deleteRound, getPlayer } from '../../shared/api'
 import AppContext from '../../shared/AppContext'
+import { SparklesIcon, TrashIcon } from 'react-native-heroicons/outline'
 
 import styles from './stats-screen.scss'
+import ComponentCard from '../../components/component-card/component-card'
 
 export default function StatsScreen({ navigation, route }) {
   const { user } = useContext(AppContext)
   const [rounds, setRounds] = useState([])
   const [isLoading, setIsLoading] = useState(true)
+  const player = getPlayer(user.name)
   useEffect(() => {
     fetchRounds()
   }, [])
@@ -22,13 +25,14 @@ export default function StatsScreen({ navigation, route }) {
   }
 
   function formatTime(dateTimeString) {
+    const monthNames = ['Jan', 'Feb', 'March', 'April', 'May', 'June', 'July', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
     const date = new Date(dateTimeString)
     const dateString =
-      date.getFullYear() +
-      '-' +
-      (date.getMonth() + 1) +
-      '-' +
       date.getDate() +
+      ' ' +
+      monthNames[date.getMonth()] +
+      ' ' +
+      date.getFullYear() +
       ' ' +
       date.getHours() +
       ':' +
@@ -40,22 +44,48 @@ export default function StatsScreen({ navigation, route }) {
     navigation.navigate('GameBreakdown', { round })
   }
 
+  function onClickDelete(roundId, player) {
+    deleteRound(roundId, player)
+  }
+
   return (
-    <View className={styles.container}>
-      {isLoading && <ActivityIndicator style={{ marginTop: '50%' }} />}
-      <ScrollView contentContainerStyle={{ alignItems: 'center' }}>
-        {!isLoading && rounds.length === 0 && <Text>You have no rounds.</Text>}
-        {!isLoading &&
-          rounds.length > 0 &&
-          rounds.map((round, i) => (
-            <View key={i}>
-              <TouchableOpacity className={styles.roundButton} onPress={() => onClickRound(round)}>
-                <Text>{formatTime(round.startTime)}</Text>
-                <Text>{round.deals.length + ' deals'}</Text>
-              </TouchableOpacity>
-            </View>
-          ))}
-      </ScrollView>
-    </View>
+    <SafeAreaView className={styles.container}>
+      <ComponentCard
+        title="Overview"
+        content={<Text style={styles.overviewText}>An overview of your games' statistics will show up here.</Text>}
+      ></ComponentCard>
+      <ComponentCard
+        title="Your games"
+        content={
+          <>
+            <ScrollView contentContainerStyle={{ alignItems: 'center' }}>
+              {isLoading && <ActivityIndicator />}
+              {!isLoading && rounds.length === 0 && <Text>You have no rounds.</Text>}
+              {!isLoading &&
+                rounds.length > 0 &&
+                rounds.map((round, i) => (
+                  <View key={i} className={styles.roundContent}>
+                    <TouchableOpacity className={styles.roundButton} onPress={() => onClickRound(round)}>
+                      <TouchableOpacity className={styles.emojiSide}>
+                        <Image
+                          className={styles.chooseEmoji}
+                          style={{ resizeMode: 'contain' }}
+                          source={require('../../assets/choose-emoji.png')}
+                        />
+                      </TouchableOpacity>
+
+                      <Text style={{ fontWeight: 'bold' }}>{formatTime(round.startTime)} </Text>
+                      <Text>{round.deals.length + ' deals'}</Text>
+                      <TouchableOpacity className={styles.binSide} onPress={onClickDelete(round._id, user.name)}>
+                        <TrashIcon color="red" style={{ marginRight: '5%' }}></TrashIcon>
+                      </TouchableOpacity>
+                    </TouchableOpacity>
+                  </View>
+                ))}
+            </ScrollView>
+          </>
+        }
+      ></ComponentCard>
+    </SafeAreaView>
   )
 }
